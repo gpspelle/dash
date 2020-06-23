81c249cc3d7070882f3f3a165ced868e49082592
gpspelle <gpsunicamp016@gmail.com>
2020-06-23 12:07:26 +0200

    combined callbacks

--- app.py
+++ app.py
@@ -6,36 +6,52 @@ from dash.dependencies import Input, Output
 import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
-
-available_indicators = df['Indicator Name'].unique()
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+all_options = {
+    'America': ['New York City', 'San Francisco', 'Cincinnati'],
+    'Canada': [u'Montréal', 'Toronto', 'Ottawa']
+}
 app.layout = html.Div([
-    dcc.Input(
-        id='num-multi',
-        type='number',
-        value=5
+    dcc.RadioItems(
+        id='countries-radio',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='America'
     ),
-    html.Table([
-        html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
-        html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
-        html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
-        html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
-        html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
-    ]),
+
+    html.Hr(),
+
+    dcc.RadioItems(id='cities-radio'),
+
+    html.Hr(),
+
+    html.Div(id='display-selected-values')
 ])
 
+
 @app.callback(
-    [Output('square', 'children'),
-     Output('cube', 'children'),
-     Output('twos', 'children'),
-     Output('threes', 'children'),
-     Output('x^x', 'children')],
-    [Input('num-multi', 'value')])
-def callback(x):
-    return x**2, x**3, 2**x, 3**x, x**x
+    Output('cities-radio', 'options'),
+    [Input('countries-radio', 'value')])
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    Output('cities-radio', 'value'),
+    [Input('cities-radio', 'options')])
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    Output('display-selected-values', 'children'),
+    [Input('countries-radio', 'value'),
+     Input('cities-radio', 'value')])
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
+
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True, port=8050)
