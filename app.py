fa60ea7c0c572ceafd143091987b4e87e9feda71
gpspelle <gpsunicamp016@gmail.com>
2020-06-23 11:58:47 +0200

    multiple output

--- app.py
+++ app.py
@@ -5,59 +5,37 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+available_indicators = df['Indicator Name'].unique()
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        step=None
-    )
+    dcc.Input(
+        id='num-multi',
+        type='number',
+        value=5
+    ),
+    html.Table([
+        html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
+        html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
+        html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
+        html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
+        html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
+    ]),
 ])
 
-
 @app.callback(
-    Output('graph-with-slider', 'figure'),
-    [Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(dict(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range':[2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition = {'duration': 500},
-        )
-    }
-
+    [Output('square', 'children'),
+     Output('cube', 'children'),
+     Output('twos', 'children'),
+     Output('threes', 'children'),
+     Output('x^x', 'children')],
+    [Input('num-multi', 'value')])
+def callback(x):
+    return x**2, x**3, 2**x, 3**x, x**x
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True, port=8050)
