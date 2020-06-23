d6ec4c47501048488e07e861b3e133bd531a8322
gpspelle <gpsunicamp016@gmail.com>
2020-06-23 11:12:05 +0200

    table example

--- app.py
+++ app.py
@@ -13,41 +13,7 @@ colors = {
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(children='Hello Dash', 
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    ''', style={
-        'textAlign': 'center',
-        'color': colors['text']}
-    ),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization',
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
-])
-
-
-'''def generate_table(dataframe, max_rows=10):
+def generate_table(dataframe, max_rows=10):
     return html.Table([
         html.Thead(
             html.Tr([html.Th(col) for col in dataframe.columns])
@@ -67,6 +33,6 @@ app.layout = html.Div(children=[
     html.H4(children='US Agriculture Exports (2011)'),
     generate_table(df)
 ])
-'''
+
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True, port=8050)
